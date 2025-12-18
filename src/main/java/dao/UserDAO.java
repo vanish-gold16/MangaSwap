ddea4b2a4bc8eb3bb2d7bbe543aecd7038526b94
Ivan Mytrofanov <mitrofanovvana20082@gmail.com>
2025-12-18 09:08:24 +0100

    Created findById(), save(), findByUsername in UserDAO.java

--- src/main/java/dao/UserDAO.java
+++ src/main/java/dao/UserDAO.java
@@ -1,7 +1,24 @@
 package dao;
 
+import entity.User;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
 public class UserDAO {
 
+    public User findByID(Session session, int id){
+        return (User) session.get(User.class, id);
+    }
 
+    public void save(Session session, User user){
+        session.persist(user);
+    }
+
+    public User findByUsername(Session session, String username){
+        return (User) session.createQuery("SELECT user " +
+                "FROM User user " +
+                "WHERE user.username = :name").
+                setParameter("name", username).uniqueResult();
+    }
 
 }
